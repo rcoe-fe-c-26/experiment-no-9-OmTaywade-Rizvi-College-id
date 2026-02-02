c25f4fd48055d5c73b1f665ed813389b3b63b693
Om Satish Taywade <omstaywade@eng.rizvi.edu.in>
2026-02-02 19:54:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,3 +7,18 @@ print("--- Factorial Finder ---\n")
 
 
 # Write your code here
+def Factorial(n):
+    if(n < 0):
+        print(f"Factorial of {-n} is Not Defined")
+    
+    elif(n == 0 or n == 1):
+        print(f"Factorial of {n} is 1")
+    
+    else:
+        fact= 1
+        for i in range(1, int(n+1)):
+            fact *= i
+        print(f"Factorial of {n} is {fact}")
+
+num = int(input("Enter Number: "))
+Factorial(num)
